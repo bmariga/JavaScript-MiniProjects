1d6e3b72f96363548ebf7aadf0e9e5f5a82fda32
mariga bruce <mariga.bn@gmail.com>
2023-04-16 20:39:26 +0300

    Update

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -5,15 +5,27 @@ var buttonColours = ["red","blue", "green", "yellow"];
 // Empty Array
 var gamePattern = [];
 
+// Empty Array
+var userClickedPattern = [];
+
+// Detect when buttons are clicked
+$(".btn").click(function() {
+
+    // UserChosenColour to store the id of the button that got clicked
+    var userChosenColour = $(this).attr("id");
+    // Add chosen colour to empty array userClickedPattern
+    userClickedPattern.push(userChosenColour);
+
+});
+
 // Function nextSequence
 function nextSequence() {
     // Random number 0 - 3 
     var randomNumber = Math.floor(Math.random() * 4); 
     // Select a random colour
     var randomChosenColour = buttonColours[randomNumber];
-    // Add random colour to empty array
+    // Add random colour to empty array gamePattern
     gamePattern.push(randomChosenColour);
-
     // Fade in Fade out 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     // Play sound when button clicked
