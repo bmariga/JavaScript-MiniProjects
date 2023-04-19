2fa2fd29907445ceb53041db95db624bf23943a9
mariga bruce <mariga.bn@gmail.com>
2023-04-19 14:39:03 +0300

    Update

---
+++ Calculator/calc.js
@@ -0,0 +1,7 @@
+(function() {
+    let screen = document.querySelector(".screen");
+    let button = document.querySelectorAll(".btn");
+    let equal = document.querySelector(".btn-equal");
+    let clear = document.querySelector(".btn-clear");
+
+});
