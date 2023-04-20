7d3b6b1ecad98cca262c9bd77f482fac9bc1fd2b
mariga bruce <mariga.bn@gmail.com>
2023-04-20 21:31:47 +0300

    Update

--- Digital Clock/clock.js
+++ Digital Clock/clock.js
@@ -5,7 +5,21 @@ function calculateTime() {
     var hour = date.getHours();
     var minute = date.getMinutes();
     var seconds = date.getSeconds();
-    
+    var ampm = hour >= 12 ? "PM" : "AM";
+    var dayNames = ["SUN","MON","TUE","WED","THUR","FRI","SAT"];
+    // Change to 12HR Format
+    hour = hour % 12;
+    // Noon not to return 0
+    hour = hour ? hour : "12";
+    // Add O infront if hour < 10
+    hour = hour < 10 ? "0" + hour : hour;
+    // Add 0 infront if minute < 10
+    minute = minute < 10 ? "0" + minute : minute;
+    // Add 0 infront if seconds < 10
+    seconds = seconds < 10 ? "0" + seconds : seconds;   
+
+
+
 }
 
 calculateTime();
