824872671582b4f8702265b5b44af9988eeee087
mariga bruce <mariga.bn@gmail.com>
2023-04-20 21:08:57 +0300

    Update

--- Digital Clock/clock.js
+++ Digital Clock/clock.js
@@ -2,10 +2,10 @@ function calculateTime() {
 
     var date = new Date();
     var dayNumber = date.getDay();
-    var hourNumber = date.getHours();
-    var minuteNumber = date.getMinutes();
-    var secondsNumber = date.getSeconds();
-    console.log(date);
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var seconds = date.getSeconds();
+    
 }
 
 calculateTime();
