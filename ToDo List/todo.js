e50293aab043c8ca343ee2caf8a572c70313c083
mariga bruce <mariga.bn@gmail.com>
2023-04-21 16:40:58 +0300

    Update

--- ToDo List/todo.js
+++ ToDo List/todo.js
@@ -8,8 +8,35 @@ input.addEventListener("keydown", function(event) {
 });
 
 function addItem() {
-      var divParent = document.createElement("div");
-      var divChild = document.createElement("div"); 
-      var trashicon = document.createElement("i");
-      var trashicon = document.createElement("i");
+    var divParent = document.createElement("div");
+    var divChild = document.createElement("div"); 
+    var checkicon = document.createElement("i");
+    var trashicon = document.createElement("i");
+
+    divParent.className = "item";
+    divParent.innerHTML = "<div>"+ input.value +"</div>";
+
+    checkicon.className = "fa-solid fa-circle-check";
+    checkicon.style.color = "black";
+    checkicon.addEventListener("click", function() {
+        checkicon.style.color = "darkgray";
+    })
+
+    divChild.appendChild(checkicon);
+
+    trashicon.className = "fas fa-trash";
+    trashicon.style.color = "black";
+    trashicon.addEventListener("click", function() {
+        divParent.remove();
+    })
+
+    divChild.appendChild(trashicon);
+
+    divParent.appendChild(divChild);
+
+    toDoItems.appendChild(divParent);
+
+    input.value = "";
+
+
 }
