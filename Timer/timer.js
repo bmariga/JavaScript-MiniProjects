39b44d4fd54892cb7afa8c9ab22c318c95a1ec21
mariga bruce <mariga.bn@gmail.com>
2023-04-22 22:52:42 +0300

    Update

---
+++ Timer/timer.js
@@ -0,0 +1,8 @@
+//  Did the clock start?
+ var timeBegan = null;
+//  At what time was the timer stopped?
+ var timeStopped = null;
+//  How long was the timer stopped?
+ var stoppedDuration = 0;
+//  
+ var startInterval = null;
