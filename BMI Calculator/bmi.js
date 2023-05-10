dacf94eb99b9af18b766c78e089c7654b6bf5ff6
Mariga <mariga.bn@gmail.com>
2023-05-10 13:41:50 +0300

    !

--- BMI Calculator/bmi.js
+++ BMI Calculator/bmi.js
@@ -1,9 +1,27 @@
 // Select button 
+
 const btn = document.getElementById("calculate");
 
 btn.addEventListener("click", () => {
 
-    let height = document.querySelector("#height");
-    let weight = document.querySelector("#weight");
-    
+    let height = document.querySelector("#height").value;
+    let weight = document.querySelector("#weight").value;
+
+// Condition to fill input fields
+
+    if(height =='' || weight == '') {
+        alert("Input fields are required");
+        return;
+    } 
+
+// BMI Calculation
+
+height = height / 100;
+
+bmi = (height / (weight ^ 2)); 
+
+
+
+
+
 });
