21f58a76d199c041250d902c74434094e8ddead8
Mariga <mariga.bn@gmail.com>
2023-05-10 13:25:32 +0300

    !

---
+++ BMI Calculator/bmi.js
@@ -0,0 +1,9 @@
+// Select button 
+const btn = document.getElementById("calculate");
+
+btn.addEventListener("click", () => {
+
+    let height = document.querySelector("#height");
+    let weight = document.querySelector("#weight");
+    
+});
